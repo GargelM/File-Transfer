e56c14b0b1b8a88e0374ad5721bd8065909efe9f
Michel Gargel <gargelmichel@gmail.com>
2020-06-27 20:07:50 -0300

    Cliando o server

--- File-Transfer/src/main/java/br/com/gargel/file/transfer/server/Server.java
+++ File-Transfer/src/main/java/br/com/gargel/file/transfer/server/Server.java
@@ -5,12 +5,69 @@
  */
 package br.com.gargel.file.transfer.server;
 
+import br.com.gargel.file.transfer.bean.ArquivoServer;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  *
  * @author Gargel
  */
 public class Server {
     public static void main(String[] args) {
+        try {
+            //1
+            ServerSocket srvSocket = new ServerSocket(22222);
+            System.out.println("Aguardando envio de arquivo ...");
+            Socket socket = srvSocket.accept();
+            
+            //2
+            byte[] objectAsByte = new byte[socket.getReceiveBufferSize()];
+            BufferedInputStream bf = new BufferedInputStream(socket.getInputStream());
+            bf.read(objectAsByte);
+            
+            //3
+            ArquivoServer arquivoS = (ArquivoServer) getObjectFromByte(objectAsByte);
+            
+            //4
+            String dir = arquivoS.getDiretorioDestino().endsWith("/") ? arquivoS.getDiretorioDestino() + arquivoS.getNome() : arquivoS.getDiretorioDestino() + "/" + arquivoS.getNome();
+            System.out.println("Escrevendo arquivo " + dir);
+            
+            //5
+            FileOutputStream fos = new FileOutputStream(dir);
+            fos.write(arquivoS.getConteudo());
+            fos.close();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Object getObjectFromByte(byte[] objectAsByte) { 
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Object obj = null;
+        ByteArrayInputStream bis = null;
+        ObjectInputStream ois = null;
         
+        try {
+            bis = new ByteArrayInputStream(objectAsByte);
+            ois = new ObjectInputStream(bis);
+            obj = ois.readObject();
+            
+            bis.close();
+            ois.close();
+            
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        
+        return obj;
     }
 }
